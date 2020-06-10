c8c8f5392d95c810c2562e589ff4b6ffcfec5a74
yalh76 <yalh@yahoo.com>
2020-06-10 03:17:46 +0200

    Get ride of sury

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,20 @@
 #=================================================
 
 # dependencies used by the app
-YNH_PHP_VERSION="7.3"
-extra_pkg_dependencies="php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-mbstring"
 pkg_dependencies="ffmpeg"
+
+YNH_PHP_VERSION="7.3"
+
+extra_php_dependencies="php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-mbstring"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
