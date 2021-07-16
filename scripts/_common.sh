2889750240c94db8aa93a01d330ad4294519c6a5
ericgaspar <junk.eg@free.fr>
2021-07-16 23:51:46 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies="ffmpeg"
 
 YNH_PHP_VERSION="7.3"
 
-extra_php_dependencies="php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-mbstring"
+extra_php_dependencies="php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
