965908f08c511f2457098abcae1ca75280c4d44f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-11 15:02:30 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION=8.0
+#REMOVEME? YNH_PHP_VERSION=8.0
 
-php_dependencies="php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-gmp php$YNH_PHP_VERSION-mbstring"
+#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-gmp php$YNH_PHP_VERSION-mbstring"
 
-pkg_dependencies="ffmpeg $php_dependencies"
+#REMOVEME? pkg_dependencies="ffmpeg $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
